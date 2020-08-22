08ff7dff96369e9b1440ad636c2c851bc7478b63
Paulo Rodrigues <pauloandreget@gmail.com>
2020-08-22 13:08:05 -0300

    Create route to home

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,25 @@
-import express from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import * as bodyParser from 'body-parser';
 
 class App {
-  public app: express.Application;
+  public app: Application;
 
   constructor() {
     this.app = express();
+
     this.setMiddlewares();
+    this.setRoutes();
   }
 
   private setMiddlewares(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
+
+  private setRoutes(): void {
+    this.app.get('/', (req: Request, res: Response, next: NextFunction) => {
+      res.send('Ok');
+    });
   }
 }
 
