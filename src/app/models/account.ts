d7f4774c48b265acaab167df12261d278cdb0931
Paulo Rodrigues <pauloandreget@gmail.com>
2020-08-23 16:53:42 -0300

    Remove mongoose-sequence and auto increment id

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,16 +1,13 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-
 export interface AccountInterface extends Document {
   balance: number;
 }
 
 const AccountSchema: Schema = new Schema({
+  id: { type: Number, required: true, unique: true },
   balance: { type: Number, required: true }
 });
-
-AccountSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
 const Account = mongoose.model<AccountInterface>('Account', AccountSchema);
 
