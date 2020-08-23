340c3ff29319a8df643bd6f14a267607e755025f
Paulo Rodrigues <pauloandreget@gmail.com>
2020-08-23 19:48:20 -0300

    Change reset response body

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,7 +12,7 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 router.post('/reset', async (req: Request, res: Response, next: NextFunction) => {
   await Account.deleteMany({}).exec();
-  res.status(httpStatus.OK).send();
+  res.status(httpStatus.OK).send('OK');
 });
 
 router.get('/balance', account.balance);
