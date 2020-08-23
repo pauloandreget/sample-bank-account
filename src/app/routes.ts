db3722410a412ec84379d98b0da8c594d81b0ef6
Paulo Rodrigues <pauloandreget@gmail.com>
2020-08-23 19:38:38 -0300

    Create route to reset collections

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,18 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import AccountController from './controllers/account';
+import httpStatus from 'http-status';
+import Account from './models/account';
 
 const router: Router = Router();
 const account: AccountController = new AccountController();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Ok');
+});
+
+router.get('/reset', async (req: Request, res: Response, next: NextFunction) => {
+  await Account.deleteMany({}).exec();
+  res.status(httpStatus.OK).send();
 });
 
 router.get('/balance', account.balance);
