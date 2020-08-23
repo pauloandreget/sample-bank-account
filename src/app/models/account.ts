049dbcccba5a3f0194d3c78c3a61b05350771aa3
Paulo Rodrigues <pauloandreget@gmail.com>
2020-08-23 12:56:20 -0300

    Add auto sequence package to id field

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
 export interface AccountInterface extends Document {
   balance: number;
 }
@@ -7,6 +9,8 @@ export interface AccountInterface extends Document {
 const AccountSchema: Schema = new Schema({
   balance: { type: Number, required: true }
 });
+
+AccountSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
 const Account = mongoose.model<AccountInterface>('Account', AccountSchema);
 
